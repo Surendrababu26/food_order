49e38b377d2d82c3c2b09668a27ce67ad4586bba
Surendra26 <yanamadalasurendra@gmai.com>
2025-10-19 18:28:23 +0530

    Your commit message here

--- first.py
+++ first.py
@@ -1,26 +1,51 @@
-print("order of the food")
-while True:
-    print("1.burger")
-    print("2.pizza")
-    choice = int(input("choose the choice(1/2): "))
-
-    if choice == 1:
-        print("your order is burger and you want any fries(yes/no)")
-        extra = input("enter your answer: ")
-        if extra == "yes":
-            print("your order is burger and fries")
+class Account:
+    def __init__(self,n,b):
+        self.name=n
+        self.balance=b
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        print(f"deposited amount is {amount}")
+        print(f"balance is{self.balance}")
+    def withdraw(self,amount):
+        if amount<=self.balance:
+            self.balance=self.balance-amount
+            print(f"withdrawl amount{amount}")
+            print(f"balance amount {self.balance}")
         else:
-            print("your order is only burger")
-        break  
-
-    elif choice == 2:
-        print("your order is pizza and you want any extra cheese(yes/no)")
-        extra = input("enter your answer: ")
-        if extra == "yes":
-            print("your order is pizza and extra cheese")
+            print("insufficient balance")
+    def show_balance(self):
+        print(f"balance {self.balance}")
+class SavingsAccount(Account):
+    def __init__(self,n,b,ir=0.05):
+        super().__init__(n,b)
+        self.interest_rate=ir
+    def add_interest(self):
+        interest=self.balance*self.interest_rate
+        self.balance=self.balance+interest
+        print(f"interest amount{interest}")
+        print(f"balance {self.balance}")
+class CheckingAccount(Account):
+    def __init__(self,n,b,ol=1000):
+        super().__init__(n,b)
+        self.overdraft_limit=ol
+    def withdraw(self,amount):
+        if self.balance+self.overdraft_limit>=amount:
+            self.balance=self.balance-amount
+            print(f"withdrawl amount{amount}")
         else:
-            print("your order is only pizza")
-        break   
+            print("exceed overdraft limit")
 
-    else:
-        print("invalid choice")
+print("========savings Account========")
+s = SavingsAccount("Surya", 1000)
+s.deposit(500)
+s.add_interest()
+s.withdraw(200)
+s.show_balance()
+print("========checking Account========")
+c = CheckingAccount("Axis", 1000)
+c.withdraw(1200)
+c.deposit(300)
+c.withdraw(700)
+c.show_balance()
+        
+    
